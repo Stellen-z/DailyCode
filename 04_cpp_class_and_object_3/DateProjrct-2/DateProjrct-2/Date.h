4a8f19359541b9e8df05f49abfa95a24ede324fb
lzh65038-web <lzh65038@gmail.com>
2026-06-15 23:03:01 +0800

    日期类-完善
    日期类-完善

---
+++ 04_cpp_class_and_object_3/DateProjrct-2/DateProjrct-2/Date.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+class Date
+{
+	//友元函数声明
+	friend ostream& operator<<(ostream& out, const Date& d);
+	friend istream& operator>>(istream& in, Date& d);
+
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+		:_year(year)
+		,_month(month)
+		,_day(day)
+	{
+		if (!CheckDate())
+		{
+			cout << "日期非法" << endl;
+		}
+	}
+
+	void Print() const;
+
+	int GetMonthDay(int year, int month) const
+	{
+		int MonthArr[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+		if ((month == 2) &&
+			((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+		{
+			return 29;
+		}
+
+		return MonthArr[month];
+	}
+
+	bool CheckDate();
+
+	bool operator<(const Date& d) const;
+	bool operator<=(const Date& d) const;
+	bool operator>(const Date& d) const;
+	bool operator>=(const Date& d) const;
+	bool operator==(const Date& d) const;
+	bool operator!=(const Date& d) const;
+
+
+	//+=
+	Date& operator+=(int day);
+
+	//-=
+	Date& operator-=(int day);
+
+	//++
+	Date& operator++();
+	Date operator++(int);
+		
+	//--
+	Date& operator--();
+	Date operator--(int);
+
+
+	//天数 - 天数
+	int operator-(const Date& d) const;
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+ostream& operator<<(ostream& out, const Date& d);
+istream& operator>>(istream& in, Date& d);
---
+++ 04_cpp_class_and_object_3/DateProjrct-2/DateProjrct-2/Test.cpp
@@ -0,0 +1,49 @@
+﻿#include "Date.h"
+
+void Test01()
+{
+	Date d1(2022, 6, 7);
+	Date d2(2022, 6, 7);
+
+	d1 += 100;
+	d1.Print();
+
+	d2 -= 200;
+	d2.Print();
+}
+
+void Test02()
+{
+	Date d1(2022, 6, 7);
+	Date d2(2022, 6, 7);
+
+	d1++;
+	d1.Print();
+
+	++d1;
+	d1.Print();
+
+	--d2;
+	d2.Print();
+
+	d2--;
+	d2.Print();
+}
+
+void Test03()
+{
+	Date d1(2022, 10, 1);
+	Date d2(2022, 6, 7);
+
+	cout << d1 - d2 << endl;
+}
+
+
+
+int main()
+{
+	//Test01();
+	//Test02();
+	Test03();
+	return 0;
+}
