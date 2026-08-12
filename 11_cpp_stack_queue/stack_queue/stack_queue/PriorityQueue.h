4285a44d217bf64a36a29b8dbcf5a846500d601b
lzh65038-web <lzh65038@gmail.com>
2026-08-12 23:23:03 +0800

    优先级队列
    优先级队列

---
+++ 11_cpp_stack_queue/stack_queue/stack_queue/PriorityQueue.h
@@ -0,0 +1,104 @@
+﻿#pragma once
+
+#include <vector>
+
+//仿函数
+template <class T>
+class Less
+{
+public:
+	//没有成员变量，大小为1字节
+	bool operator()(const T& x, const T& y)
+	{
+		return x < y;
+	}
+};
+
+template <class T>
+class Greater
+{
+public:
+	//没有成员变量，大小为1字节
+	bool operator()(const T& x, const T& y)
+	{
+		return x > y;
+	}
+};
+
+
+namespace stl
+{
+	//类模板参数传类型，函数参数传对象
+	//默认建大堆，升序
+	template<class T, class Container = std::vector<T>,class Compare = Less<T>>
+	class priority_queue
+	{
+	private:
+		Container _con;
+	public:
+
+		void AdjustUp(size_t child)
+		{
+			Compare _cmp;
+			size_t parent = (child - 1) / 2;
+			while (child > 0)
+			{
+				//if (_con[parent] < _con[child])
+				if (_cmp(_con[parent],_con[child]))
+				{
+					std::swap(_con[parent], _con[child]);
+					child = parent;
+					parent = (child - 1) / 2;
+				}
+				else break;
+			}
+		}
+
+		void push(const T& x)
+		{
+			_con.push_back(x);
+			AdjustUp(_con.size() - 1);
+		}
+
+		void AdjustDown(int parent)
+		{
+			Compare _cmp;
+			int child = parent * 2 + 1;
+			while (child < _con.size())
+			{
+				if (child + 1 < _con.size() && _cmp(_con[child],_con[child + 1])) ++child;
+				//if (_con[parent] < _con[child])
+				if (_cmp(_con[parent],_con[child]))
+				{
+					std::swap(_con[child], _con[parent]);
+					parent = child;
+					child = parent * 2 + 1;
+				}
+				else break;
+			}
+		}
+
+		void pop()
+		{
+			std::swap(_con[0], _con[_con.size() - 1]);
+			_con.pop_back();
+			AdjustDown(0);
+		}
+
+		const T& top()
+		{
+			return _con[0];
+		}
+
+		const size_t size() const
+		{
+			return _con.size();
+		}
+
+		bool empty()
+		{
+			return _con.empty();
+		}
+
+	};
+}
--- 11_cpp_stack_queue/stack_queue/stack_queue/Test.cpp
+++ 11_cpp_stack_queue/stack_queue/stack_queue/Test.cpp
@@ -2,9 +2,12 @@
 #include <vector>
 #include <list>
 #include <deque>
+#include <queue>
 
 #include "Stack.h"
 #include "Queue.h"
+#include "PriorityQueue.h"
+//#include "PriorityQueue.h"
 
 using namespace std;
 
@@ -45,8 +48,51 @@ namespace stl
 
 	}
 
-	void test_deque()
+
+
+
+	template<class Com>
+	void BubbleSort(int* a, int n,Com cmp)
 	{
+		for (int j = 0; j < n - 1; j++)
+		{
+			int flag = 1;
+			for (int i = 0; i < n - 1 - j; i++)
+			{
+				//if (a[i] < a[i - 1])
+				if (cmp(a[i + 1],a[i]))
+				{
+					swap(a[i], a[i + 1]);
+					flag = 0;
+				}
+			}
+			if (flag == 1) break;
+		}
+		
+	}
+
+	void test_priority_queue()
+	{
+		//测试仿函数
+		//int a[] = { 1,5,7,2,8,4,0,3,6 };
+		//BubbleSort(a, sizeof(a) / sizeof(int), Less<int>());
+		//BubbleSort(a, sizeof(a) / sizeof(int), Greater<int>());
+
+		//priority_queue<int> pq;
+		priority_queue<int,vector<int>,greater<int>> pq;
+
+		pq.push(1);
+		pq.push(3);
+		pq.push(4);
+		pq.push(6);
+		pq.push(8);
+
+		while (!pq.empty())
+		{
+			std::cout << pq.top() << " ";
+			pq.pop();
+		}
+		std::cout << std::endl;
 
 	}
 
@@ -55,9 +101,9 @@ namespace stl
 
 int main()
 {
-	stl::test_stack();
-	stl::test_queue();
-	stl::test_deque();
+	//stl::test_stack();
+	//stl::test_queue();
+	stl::test_priority_queue();
 
 }
 
