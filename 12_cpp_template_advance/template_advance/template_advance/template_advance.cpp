d7c4abf69d322b86238d363787b70a7c07b034fd
lzh65038-web <lzh65038@gmail.com>
2026-08-16 23:53:50 +0800

    模板进阶
    模板进阶

---
+++ 12_cpp_template_advance/template_advance/template_advance/template_advance.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<size_t N = 2>
+class Stack
+{
+private:
+	int _a[N];
+	int _top;
+	int _size;
+};
+
+
+
+
+int main()
+{
+	Stack<> st0;
+	Stack<5> st1;
+	Stack<10> st2;
+
+	return 0;
+}
