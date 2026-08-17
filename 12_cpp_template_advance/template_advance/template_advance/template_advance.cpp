a4d101926c3eaef04ef8a2e8b46ff52148d95fa6
lzh65038-web <lzh65038@gmail.com>
2026-08-17 22:12:39 +0800

    模板进阶
    模板进阶

--- 12_cpp_template_advance/template_advance/template_advance/template_advance.cpp
+++ 12_cpp_template_advance/template_advance/template_advance/template_advance.cpp
@@ -1,24 +1,106 @@
 ﻿#include <iostream>
+#include <string>
+	
+#include "template_advance.h"
 
-using namespace std;
-
-template<size_t N = 2>
-class Stack
+namespace stl
 {
-private:
-	int _a[N];
-	int _top;
-	int _size;
-};
+	//void test_template_value()
+	//{
+	//	//通过传入的N的大小来控制底层的_a
+	//	Stack<> st0;
+	//	Stack<5> st1;
+	//	Stack<10> st2;
+	//}
+
+
+	void test_array()
+	{
+		array<int,1> a1;
+		for (int i = 0; i < 1; i++)
+		{
+			a1[i] = i;
+			std::cout << a1[i] << " ";
+		}
+		std::cout << std::endl;
+
+		array<int,5> a2;
+		for (int i = 0; i < 5; i++)
+		{
+			a2[i] = i;
+			std::cout << a2[i] << " ";
+		}
+		std::cout << std::endl;
+
+		array<int> a3;
+		for (int i = 0; i < 10; i++)
+		{
+			a3[i] = i;
+			std::cout << a3[i] << " ";
+		}
+		std::cout << std::endl;
+
+	}
+
+	void test_compare()
+	{
+		int a = 1;
+		int b = 2;
+		std::cout << less(a, b) << std::endl;
+
+		std::string s1 = "hello";
+		std::string s2 = "world";
+		std::cout << less(s1, s2) << std::endl;
+
+		std::string* p1 = &s1;
+		std::string* p2 = &s2;
+		//此时比较的是什么？
+		std::cout << less(p1, p2) << std::endl;
+
+	}
+
+	void test_specialize()
+	{
+		example<char, char> e1;       //模板
+		example<int, char> e2;        //全特化
+		example<std::string, int> e3; //偏特化
+
+		example<int*, int*> e4;       //偏特化
+		example<char*, char*> e5;     //偏特化
+		example<int&, int&> e6(1,2);  //偏特化
+
+	}
+
+	//Add函数定义
+	template<class T>
+	T Add(const T& x, const T& y)
+	{
+		return x + y;
+	}
+
+	void test_add()
+	{
+		std::cout << Add(1, 2) << std::endl;
+		std::cout << Add(1.1, 2.2) << std::endl;
+
+	}
+
+
+}
+
+
+
 
 
 
 
 int main()
 {
-	Stack<> st0;
-	Stack<5> st1;
-	Stack<10> st2;
+	//stl::test_template_value();
+	//stl::test_array();
+	//stl::test_compare();
+	//stl::test_specialize();
+	stl::test_add();
 
 	return 0;
 }
---
+++ 12_cpp_template_advance/template_advance/template_advance/template_advance.h
@@ -0,0 +1,151 @@
+﻿#pragma once
+
+namespace stl
+{
+	//N即为非类型模板参数
+	template<size_t N = 2>
+	class Stack
+	{
+	private:
+		int _a[N];
+		int _top;
+		int _size;
+	};
+
+	template<class T, size_t N = 10>
+	class array
+	{
+	public:
+		T& operator[](size_t pos)
+		{
+			return _array[pos];
+		}
+
+		const T& operator[](size_t pos) const
+		{
+			return _array[pos];
+		}
+
+		size_t size()
+		{
+			return _size;
+		}
+
+		bool ematy()
+		{
+			return _size == 0;
+		}
+
+	private:
+		size_t _size = 0;
+		T _array[N];//N的大小来控制静态数组的大小
+	};
+
+	template<class T>
+	bool less(const T& x,const T& y)
+	{
+		return x < y;
+	}
+	
+	//特化
+	template<>
+	bool less<std::string*>(std::string* const & x, std::string* const & y)
+	{
+		return *x < *y;
+	}
+
+	bool less(std::string* const& x, std::string* const& y)
+	{
+		return *x < *y;
+	}
+
+	template<class T1,class T2>
+	class example
+	{
+	public:
+		example()
+		{
+			std::cout << "example<T1, T2>" << std::endl;
+		}
+	private:
+		T1 _a;
+		T2 _b;
+	};
+
+	template<>
+	class example<int,char>
+	{
+	public:
+		example()
+		{
+			std::cout << "example<int, char>" << std::endl;
+		}
+	private:
+		int _a;
+		char _b;
+	};
+
+	//偏特化
+	template<class T>
+	class example<T,int>
+	{
+	public:
+		example()
+		{
+			std::cout << "example<T,int>" << std::endl;
+		}
+	private:
+		T _a;
+		int _b;
+	};
+
+	//参数偏特化为指针
+	template<class T1,class T2>
+	class example<T1*,T2*>
+	{
+	public:
+		example()
+		{
+			std::cout << "example<T1*,T2*>" << std::endl;
+		}
+	private:
+		T1* _a;
+		T2* _b;
+	};
+
+	//参数偏特化为引用
+	template<class T1, class T2>
+	class example<T1&, T2&>
+	{
+	public:
+		example(const T1& _a, const T2& _b)
+			:_a(_a)
+			,_b(_b)//引用必须要初始化
+		{
+			std::cout << "example<T1&, T2&>" << std::endl;
+		}
+	private:
+		
+		const T1& _a;
+		const T2& _b;
+	};
+	
+	//声明
+	template<class T>
+	T Add(const T& x, const T& y);
+	
+	//显示实例化
+	template int Add(const int&, const int&);
+	template double Add(const double&, const double&);
+
+
+	//声明+定义
+	//template<class T>
+	//T Add(const T& x, const T& y)
+	//{
+	//	return x + y;
+	//}
+
+
+
+}
