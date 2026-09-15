745cd57df30ad3b0e75c761cb37be1ec5dd9de5e
lzh65038-web <lzh65038@gmail.com>
2026-09-15 22:46:23 +0800

    C++11

--- 20_cpp_c++11/C++11/C++11/C++11.cpp
+++ 20_cpp_c++11/C++11/C++11/C++11.cpp
@@ -1,6 +1,12 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<assert.h>
+#include<string.h>
+#include<algorithm>
 
 using namespace std;
+
 
 class Date
 {
@@ -40,12 +46,207 @@ void test1()
 
 	d1.Print();
 	d2.Print();
+}
+
+namespace stl
+{
+class string
+{
+public:
+	typedef char* iterator;
+	typedef const char* const_iterator;
+	iterator begin()
+	{
+		return _str;
+	}
+	iterator end()
+	{
+		return _str + _size;
+	}
+	const_iterator begin() const
+	{
+		return _str;
+	}
+	const_iterator end() const
+	{
+		return _str + _size;
+	}
+	string(const char* str = "")
+		:_size(strlen(str))
+		, _capacity(_size)
+	{
+		cout << "string(char* str)-构造" << endl;
+		_str = new char[_capacity + 1];
+		strcpy(_str, str);
+	}
+	void swap(string& s)
+	{
+		::swap(_str, s._str);
+		::swap(_size, s._size);
+		::swap(_capacity, s._capacity);
+	}
+	string(const string& s)
+		:_str(nullptr)
+	{
+		cout << "string(const string& s) -- 拷贝构造" << endl; reserve(s._capacity);
+		for (auto ch : s)
+		{
+			push_back(ch);
+		}
+	}
+	// 移动构造​
+	//string(string&& s)
+	//{
+	//	cout << "string(string&& s) -- 移动构造" << endl;
+	//	swap(s);
+	//}
+	string& operator=(const string& s)
+	{
+		cout << "string& operator=(const string& s) -- 拷贝赋值" <<
+			endl;
+		if (this != &s)
+		{
+			_str[0] = '\0';
+			_size = 0;
+			reserve(s._capacity);
+			for (auto ch : s)
+			{
+				push_back(ch);
+			}
+		}
+		return *this;
+	}
+	// 移动赋值​
+	//string& operator=(string&& s)
+	//{
+	//	cout << "string& operator=(string&& s) -- 移动赋值" << endl;
+	//	swap(s);
+	//	return *this;
+	//}
+
+	//~string()
+	//{
+	//	cout << "~string() -- 析构" << endl;
+	//	delete[] _str;
+	//	_str = nullptr;
+	//}
+	
+	char& operator[](size_t pos)
+	{
+		assert(pos < _size);
+		return _str[pos];
+	}
+	void reserve(size_t n)
+	{
+		if (n > _capacity)
+		{
+			char* tmp = new char[n + 1];
+			if (_str)
+			{
+				strcpy(tmp, _str);
+				delete[] _str;
+			}
+			_str = tmp;
+			_capacity = n;
+		}
+	}
+	void push_back(char ch)
+	{
+		if (_size >= _capacity)
+		{
+			size_t newcapacity = _capacity == 0 ? 4 : _capacity *
+				2;
+			reserve(newcapacity);
+		}
+		_str[_size] = ch;
+		++_size;
+		_str[_size] = '\0';
+	}
+	string& operator+=(char ch)
+	{
+		push_back(ch);
+		return *this;
+	}
+	const char* c_str() const
+	{
+		return _str;
+	}
+	size_t size() const
+	{
+		return _size;
+	}
+private:
+	char* _str = nullptr;
+	size_t _size = 0;
+	size_t _capacity = 0;
+};
+
+	string addStrings(string num1, string num2)
+	{
+		string str;
+		int end1 = num1.size() - 1, end2 = num2.size() - 1;
+		int next = 0;
+		while (end1 >= 0 || end2 >= 0)
+		{
+			int val1 = end1 >= 0 ? num1[end1--] - '0' : 0;
+			int val2 = end2 >= 0 ? num2[end2--] - '0' : 0;
+			int ret = val1 + val2 + next;
+			next = ret / 10;
+			ret = ret % 10;
+			str += ('0' + ret);
+		}
+		if (next == 1)
+			str += '1';
+		reverse(str.begin(), str.end());
+		//cout << "******************************" << endl;
+		return str;
+	}
 
 }
 
+template<class T>
+void Function(T&& t)
+{
+	int a = 0;
+	T x = a;
+	//x++;
+	cout << &a << endl;
+	cout << &x << endl << endl;
+}
 
 int main()
 {
-	test1();
+	//test1();
+
+	//stl::string ret = stl::addStrings("11111", "2222");
+	//cout << ret.c_str() << endl;
+
+	stl::string ret;
+	ret = stl::addStrings("11111", "2222");
+	cout << ret.c_str() << endl;
+
+	
+	// 10是右值，推导出T为int，模板实例化为void Function(int&& t)​
+	Function(10); // 右值​
+
+	int a;
+	// a是左值，推导出T为int&，引用折叠，模板实例化为void Function(int& t)​
+	Function(a); // 左值​
+
+	// std::move(a)是右值，推导出T为int，模板实例化为void Function(int&& t)​
+	Function(std::move(a)); // 右值​
+
+	const int b = 8;
+	// b是左值，推导出T为const int&，引用折叠，模板实例化为void Function(const int&t)
+	// 所以Function内部会编译报错，x不能++​
+	Function(b); // const 左值
+
+
+	//std::move(b)右值，推导出T为const int，模板实例化为void Function(const int&&t)
+	// 所以Function内部会编译报错，x不能++​
+	Function(std::move(b)); // const 右值​
+
 	return 0;
 }
+
+
