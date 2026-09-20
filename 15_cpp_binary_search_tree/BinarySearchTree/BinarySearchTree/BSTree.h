43ae8177218a83bc0e9749bd50fa805089256f30
lzh65038-web <lzh65038@gmail.com>
2026-09-20 23:27:58 +0800

    二叉搜索树
    二叉搜索树

---
+++ 15_cpp_binary_search_tree/BinarySearchTree/BinarySearchTree/BSTree.h
@@ -0,0 +1,182 @@
+﻿#pragma once
+#include <iostream>
+
+namespace stl
+{
+	template<class K>
+	struct BSTNode
+	{
+		BSTNode(const K& key)
+			:_left(nullptr)
+			, _right(nullptr)
+			, _key(key)
+		{}
+
+		BSTNode<K>* _left;
+		BSTNode<K>* _right;
+		K _key;
+	};
+
+	template<class K>
+	class BST
+	{
+	public:
+		using Node = BSTNode<K>;
+
+		BST() = default;
+
+		void InOrder()
+		{
+			_InOrder(_root);
+			std::cout << std::endl;
+		}
+
+		bool Insert(const K& key)
+		{
+			if (_root == nullptr)
+			{
+				_root = new Node(key);
+				return true;
+			}
+
+			//找到合适位置
+			Node* cur = _root;
+			Node* parent = nullptr;
+			while (cur)
+			{
+				if (key > cur->_key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (key < cur->_key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			//prev即为其父节点
+			Node* newnode = new Node(key);
+			if (key > parent->_key) parent->_right = newnode;
+			else parent->_left = newnode;
+
+			return true;
+
+		}
+
+		bool Find(const K& key)
+		{
+			Node* cur = _root;
+			while (cur)
+			{
+				if (key > cur->_key)
+				{
+					cur = cur->_right;
+				}
+				else if (key < cur->_key)
+				{
+					cur = cur->_left;
+				}
+				else
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		bool Erase(const K& key)
+		{
+			Node* cur = _root;
+			Node* parent = nullptr;
+			while (cur)
+			{
+				if (key > cur->_key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (key < cur->_key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else
+				{
+					if (cur->_left == nullptr)
+					{
+						if (parent == nullptr)
+						{
+							_root = cur->_right;
+						}
+						else
+						{
+							if (parent->_left == cur) parent->_left = cur->_right;
+							else parent->_right = cur->_right;
+						}
+					
+						delete cur;
+						return true;
+					}
+					else if (cur->_right == nullptr)
+					{
+						if (parent == nullptr)
+						{
+							_root = cur->_left;
+						}
+						else
+						{
+							if (parent->_left == cur) parent->_left = cur->_left;
+							else parent->_right = cur->_left;
+						}
+
+						delete cur;
+						return true;
+					}
+					else
+					{
+						// 右子树的最小值
+						Node* RightMinP = cur;
+						Node* RightMin = cur->_right;
+						while (RightMin->_left)
+						{
+							RightMinP = RightMin;
+							RightMin = RightMin->_left;
+						}
+						
+						cur->_key = RightMin->_key;
+
+						if (RightMinP->_right == RightMin) RightMinP->_right = RightMin->_right;
+						else RightMinP->_left = RightMin->_right;
+
+						delete RightMin;
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+
+	private:
+		void _InOrder(Node* root)
+		{
+			if (root == nullptr) return;
+
+			_InOrder(root->_left);
+			std::cout << root->_key << " ";
+			_InOrder(root->_right);
+		}
+
+		BSTNode<K>* _root = nullptr;
+	};
+
+}
+
--- 15_cpp_binary_search_tree/BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
+++ 15_cpp_binary_search_tree/BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
@@ -1,194 +1,74 @@
-﻿#include <iostream>
+﻿#include "BSTree.h"
 
-using namespace std;
-
-template<class K>
-struct BSTNode
+namespace stl
 {
-	BSTNode(const K& key)
-		:_left(nullptr)
-		,_right(nullptr)
-		,_key(key)
-	{ }
-
-	BSTNode<K>* _left;
-	BSTNode<K>* _right;
-	K _key;
-};
-
-template<class K>
-class BST
-{
-public:
-	using Node = BSTNode<K>;
-
-	BST() = default;
-
-	BST(const BST& val)
+	void test_insert()
 	{
-		Node* cur = 
-		while()
+		int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+		BST<int> bt;
+		for (auto& e : a)
+		{
+			bt.Insert(e);
+		}
+	
+		bt.InOrder();
+
+		std::cout << bt.Find(1) << std::endl;
+		std::cout << bt.Find(3) << std::endl;
+		std::cout << bt.Find(100) << std::endl;
+		std::cout << bt.Find(6) << std::endl;
+		std::cout << bt.Find(7) << std::endl;
+
 	}
 
-	bool insert(const K& key)
+
+	void test_erase()
 	{
-		if (_root == nullptr)
+		int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+		BST<int> bt;
+		for (auto& e : a)
 		{
-			_root = new Node(key);
-			return true;
-		}	
-
-		Node* cur = _root;
-		Node* parent = nullptr;
-
-		while (cur)
-		{
-			if (key > cur->_key)
-			{
-				parent = cur;
-				cur = cur->_right;
-			}
-			else if (key < cur->_key)
-			{
-				parent = cur;
-				cur = cur->_left;
-			}
-			else
-			{
-				//相等就不能插入
-				return false;
-			}
+			bt.Insert(e);
 		}
 
-		cur = new Node(key);
-		if (key > parent->_key) parent->_right = cur;
-		else parent->_left = cur;
-		return true;
+		bt.InOrder();		
+
+		bt.Erase(3);
+		bt.InOrder();
+
+		bt.Erase(4);
+		bt.InOrder();
+
+		bt.Erase(8);
+		bt.InOrder();
 	}
 
-	bool find(const K& key)
-	{
-		Node* cur = _root;
-		while (cur)
-		{
-			if (key > cur->_key) cur = cur->_left;
-			else if (key < cur->_key) cur = cur->_right;
-			else return true;
-		}
 
-		return false;
-	}
 
-	void InOrder()
-	{
-		_InOrder(_root);
-		cout << endl;
-	}
-
-	bool erase(const K& key)
-	{
-		Node* cur = _root;
-		Node* parent = nullptr;
-		while (cur)
-		{
-			if (key > cur->_key)
-			{
-				parent = cur;
-				cur = cur->_right;
-			}
-			else if (key < cur->_key)
-			{
-				parent = cur;
-				cur = cur->_left;
-			}
-			else
-			{
-				//右孩子为空
-				if (cur->_right == nullptr)
-				{
-					if (parent == nullptr)
-					{
-						_root = cur->_left;
-					}
-					else
-					{
-						if (parent->_left == cur) parent->_left = cur->_left;
-						else parent->_right = cur->_left;
-					}
-				
-					delete cur;
-					return true;
-				}
-				else if (cur->_left == nullptr)
-				{
-					if (parent == nullptr)
-					{
-						_root = cur->_right;
-					}
-					else
-					{
-						if (parent->_left == cur) parent->_left = cur->_right;
-						else parent->_right = cur->_right;
-					}
-
-					delete cur;
-					return true;
-				}
-				else //两个孩子均不为空
-				{
-					//找到右子树的最左节点
-					Node* RightMinP = cur;
-					Node* RightMin = cur->_right;
-
-					while (RightMin->_left)
-					{
-						RightMinP = RightMin;
-						RightMin = RightMin->_left;
-					}
-
-					cur->_key = RightMin->_key;
-
-					if(RightMinP->_left == RightMin) RightMinP->_left = RightMin->_right;
-					else  RightMinP->_right = RightMin->_right;
-					
-					delete RightMin;
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-private:
-
-	void _InOrder(Node* root)
-	{
-		if (root == nullptr) return;
-		
-		_InOrder(root->_left);
-		cout << root->_key << " ";
-		_InOrder(root->_right);
-	}
-
-	BSTNode<K>* _root = nullptr;
-};
+}
 
 int main()
 {
-	BST<int> t;
-	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
-	for (auto e : a)
-	{
-		t.insert(e);
-	}
-
-	t.InOrder();
-
-	t.erase(8);
-	t.InOrder();
-
-
-
+	//stl::test_insert();
+	stl::test_erase();
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
